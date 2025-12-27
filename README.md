5c5d388718f6700ec5b043ada83974ceeca4bd74
imsudipcreator <imsudipmahata@gmail.com>
2025-12-27 11:11:08 +0530

    initial release with playwright and vitest setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Happy Mart
+A online ecommerce platform for buying products.
+
---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const InterFont = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+export const metadata: Metadata = {
+  title: "Happy Mart - Online Ecommerce Platform",
+  description: "A online ecommerce platform for buying products",
+  icons: {
+    icon: "/logo.svg"
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${InterFont.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
